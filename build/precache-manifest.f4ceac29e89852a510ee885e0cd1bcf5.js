283a830a613dd35a516cc3f138ccf9716132c04b
damon yan <stylekn@hotmail.com>
2022-10-13 19:58:02 +1100

    first build

---
+++ build/precache-manifest.f4ceac29e89852a510ee885e0cd1bcf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd92fc0d37278790d7fecd45aac1f82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b6bc3aef9050e1bd3ec",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "8b6bc3aef9050e1bd3ec",
+    "url": "/static/js/2.de44b62a.chunk.js"
+  },
+  {
+    "revision": "df845dd979b32831af9a5453d0f01b16",
+    "url": "/static/js/2.de44b62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77bc21d629568e8029ef",
+    "url": "/static/js/main.74c61f81.chunk.js"
+  },
+  {
+    "revision": "488cf687ed620398670f",
+    "url": "/static/js/runtime-main.b34b1e44.js"
+  }
+]);
